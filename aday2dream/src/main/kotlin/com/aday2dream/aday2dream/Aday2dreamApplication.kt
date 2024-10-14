824bfd0b7d93e33c053958412de48be8da76093b
yoskohaha <hourbin86@gmail.com>
2024-10-14 15:35:20 +0300

    Initial commit

---
+++ aday2dream/src/main/kotlin/com/aday2dream/aday2dream/Aday2dreamApplication.kt
@@ -0,0 +1,11 @@
+package com.aday2dream.aday2dream
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Aday2dreamApplication
+
+fun main(args: Array<String>) {
+	runApplication<Aday2dreamApplication>(*args)
+}
---
+++ aday2dream/src/test/kotlin/com/aday2dream/aday2dream/Aday2dreamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aday2dream.aday2dream
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Aday2dreamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
